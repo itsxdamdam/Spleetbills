4eb9e40f18e1ade8c01d380e04871d15a48593dc
thesaam <samuel@wedeydo.com>
2022-07-20 20:35:43 +0100

    fixes

--- app.js
+++ app.js
@@ -105,6 +105,7 @@ app.post('/split-payments/compute', (req, res) => {
   res.status(200).json({result})
 })
 
-app.listen(3000, () => {
+
+app.listen(5000, () => {
   console.log("Server is running on port 3000...");
 })
